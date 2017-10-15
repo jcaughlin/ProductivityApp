3ab661b80a16987dd530b23798f2fd32829b0b3f
JS Caughlin <jcaughlin@madisoncollege.edu>
2017-10-15 17:19:10 -0500

    Working on Hibernate Deleted database properties file

--- src/main/java/edu/matc/persistence/UserDao.java
+++ src/main/java/edu/matc/persistence/UserDao.java
@@ -6,6 +6,8 @@ import org.hibernate.Criteria;
 import org.hibernate.HibernateException;
 import org.hibernate.Session;
 import org.hibernate.Transaction;
+import org.hibernate.criterion.Projection;
+import org.hibernate.criterion.Projections;
 import org.hibernate.criterion.Restrictions;
 
 import java.util.ArrayList;
@@ -15,97 +17,6 @@ public class UserDao {
 
 
     private final Logger log = Logger.getLogger(this.getClass());
-
-    /**
-     * Return a list of all users
-     *
-     * @return All users
-     */
-    public List<User> getAllUsers() {
-        List<User> users = new ArrayList<User>();
-        Session session = null;
-        try {
-            session = SessionFactoryProvider.getSessionFactory().openSession();
-            users = session.createCriteria(User.class).list();
-        } catch (HibernateException he) {
-            log.error("Error getting all users", he);
-        } finally {
-            if (session != null) {
-                session.close();
-            }
-        }
-        return users;
-    }
-
-    /**
-     * Get a single user for the given id
-     *
-     * @param id user's id
-     * @return User
-     */
-    public User getUser(int id) {
-        User user = null;
-        Session session = null;
-        try {
-            session = SessionFactoryProvider.getSessionFactory().openSession();
-            user = (User) session.get(User.class, id);
-        } catch (HibernateException he) {
-            log.error("Error getting user with id: " + id, he);
-        } finally {
-            if (session != null) {
-                session.close();
-            }
-        }
-        return user;
-    }
-
-    /**
-     * Retrieve users by lastname
-     *
-     * @param lastName User's last name which is the search criteria
-     * @return User
-     */
-    public List<User> getUsersByLastName(String lastName) {
-        List<User> users = new ArrayList<User>();
-        Session session = null;
-        try {
-            session = SessionFactoryProvider.getSessionFactory().openSession();
-            Criteria criteria = session.createCriteria(User.class);
-            criteria.add(Restrictions.eq("lastName", lastName));
-            users = criteria.list();
-        } catch (HibernateException he) {
-            log.error("Error getting all users with last name: " + lastName, he);
-        } finally {
-            if (session != null) {
-                session.close();
-            }
-        }
-        return users;
-    }
-
-    /**
-     * Gets a user from the database
-     *
-     * @param user_name The username to find
-     * @return a User
-     */
-    public User getUserByUserName(String user_name) {
-        User user = null;
-        Session session = null;
-
-        try {
-            session = SessionFactoryProvider.getSessionFactory().openSession();
-            user = (User) session.get(User.class, user_name);
-        } catch (HibernateException he) {
-            log.error("Error retrieving username: " + user_name, he);
-        } finally {
-            if (session != null) {
-                session.close();
-            }
-        }
-
-        return user;
-    }
 
     /**
      * save new user
@@ -126,7 +37,7 @@ public class UserDao {
         } catch (HibernateException he) {
             if (transaction != null) {
                 try {
-                    transaction.rollback();x``
+                    transaction.rollback();
                 } catch (HibernateException he2) {
                     log.error("Error rolling back save of user: " + user, he2);
                 }
@@ -137,6 +48,37 @@ public class UserDao {
             }
         }
         return id;
+    }
+
+    /**
+     * Remove user
+     *
+     * @param user
+     */
+    public void removeUser(User user) {
+        Transaction transaction = null;
+        Session session = null;
+
+        try {
+            log.info(user.toString());
+            session = SessionFactoryProvider.getSessionFactory().openSession();
+            transaction = session.beginTransaction();
+            session.delete(user);
+            log.info(user.toString());
+            transaction.commit();
+        } catch (HibernateException he) {
+            if (transaction != null) {
+                try {
+                    transaction.rollback();
+                } catch (HibernateException he2) {
+                    log.error("error rolling back deletion of User: " + user, he2);
+                }
+            }
+        } finally {
+            if (session != null) {
+                session.close();
+            }
+        }
     }
 
     /**
@@ -169,33 +111,73 @@ public class UserDao {
     }
 
     /**
-     * Remove user
+     * Return a list of all users
      *
-     * @param user
+     * @return All users
      */
-    public void removeUser(User user) {
-        Transaction transaction = null;
+    public List<User> getAllUsers() {
+        List<User> users = new ArrayList<User>();
         Session session = null;
-
         try {
             session = SessionFactoryProvider.getSessionFactory().openSession();
-            transaction = session.beginTransaction();
-            session.delete(user);
-            transaction.commit();
+            users = session.createCriteria(User.class).list();
         } catch (HibernateException he) {
-            if (transaction != null) {
-                try {
-                    transaction.rollback();
-                } catch (HibernateException he2) {
-                    log.error("error rolling back deletion of User: " + user, he2);
-                }
-            }
+            log.error("Error getting all users", he);
         } finally {
             if (session != null) {
                 session.close();
             }
         }
+        return users;
     }
+
+
+    /**
+     * Retrieve users by lastname
+     *
+     * @param lastName User's last name which is the search criteria
+     * @return User
+     */
+    public List<User> getUsersByLastName(String lastName) {
+        List<User> users = new ArrayList<User>();
+        Session session = null;
+        try {
+            session = SessionFactoryProvider.getSessionFactory().openSession();
+            Criteria criteria = session.createCriteria(User.class);
+            criteria.add(Restrictions.eq("userLastName", lastName));
+            users = criteria.list();
+        } catch (HibernateException hibernateException) {
+            log.error("Error getting all users with last name: " + lastName, hibernateException);
+        } finally {
+            if (session != null) {
+                session.close();
+            }
+        }
+        return users;
+    }
+
+    /**
+     * Get a single user for the given id
+     *
+     * @param id user's id
+     * @return User
+     */
+    public User getUserById(int id) {
+        User user = null;
+        Session session = null;
+        try {
+            session = SessionFactoryProvider.getSessionFactory().openSession();
+            user = (User) session.get(User.class, id);
+        } catch (HibernateException he) {
+            log.error("Error getting user with id: " + id, he);
+        } finally {
+            if (session != null) {
+                session.close();
+            }
+        }
+        return user;
+    }
+
 
     /**
      * Query user by email
@@ -220,6 +202,30 @@ public class UserDao {
         }
         return user;
     }
+
+    /**
+     * Return number of records
+     *
+     * @return int recordCount the number of records
+     */
+    public int getRecordCount() {
+        Session session = null;
+        Transaction transaction = null;
+
+
+
+            session = SessionFactoryProvider.getSessionFactory().openSession();
+        Criteria crit = session.createCriteria(User.class);
+        crit.add( Restrictions.isNotNull(user.getUserId()));
+        crit.setProjection(Projections.rowCount());
+        Integer count = (Integer)crit.uniqueResult();
+        log.info("The count is: " + count);
+
+        return count;
+
+
+    }
+
 
 }
 
